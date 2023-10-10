45c9f909ebacd2f3701154632e48340dafff7ed9
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-10-10 20:20:16 +0200

    .

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,7 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor(public name: string, private department: string, protected salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +29,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(salary: number) {
+    super('John','department', salary + 10000);
+  }
 }
-
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[];
+  protected door: boolean;
+
+  constructor(protected key: Key) {
+    this.key = key;
+  }
+
+  comeIn(newTenant: Person) {
+    if (this.door) {
+      this.tenants.push(newTenant);
+    }
+  }
+
+  abstract openDoor(keyToEnterTheHouse: Key): void;
+}
+
+class MyHouse extends House {
+  
+  openDoor(keyToEnterTheHouse: Key): void {
+    if (this.key === keyToEnterTheHouse) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
